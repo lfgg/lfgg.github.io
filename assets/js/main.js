3cc2505d2033c2b03c1b5a23a66a4ec8f91166cd
Keith Pickering <thatonekeithguy@gmail.com>
2018-12-03 10:42:17 -0800

    Transition on initial page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,10 @@ const Page = {
             this.elems.loader.parentNode.replaceChild(newLoader, this.elems.loader);
             this.elems.loader = newLoader;
         });
+
+        window.addEventListener('load', () => {
+            document.documentElement.classList.remove('is-animating');
+        });
     }
 }
 
